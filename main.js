56776b1a834ec0ece8e99a5c91498c46fede3e98
saish9760 <81807449+saish9760@users.noreply.github.com>
2021-05-08 15:49:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var firstname="Saish"
+var lastname="Kandalgaonkar"
+var fullname= firstname.concat(lastname)
+console.log(fullname)
+var a=56
+var b=64
+var sum=a+b
+var product=a*b
+console.log ("sum")
+console.log  (sum)
+console.log  ("product")
+console.log  (product)
